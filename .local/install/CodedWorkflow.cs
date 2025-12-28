519fb40692898ed9a6a754444cf0e87ea7011db5
Zamfir Andreea Tamina <taminazamfir@gmail.com>
2025-12-28 20:19:57 +0200

    Test commit github

--- .local/install/CodedWorkflow.cs
+++ .local/install/CodedWorkflow.cs
@@ -20,7 +20,7 @@ namespace ProiectRPAZamfirTamina
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
