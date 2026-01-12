cc100e202acf28bab9e21cacace86933c2ca68b9
Zamfir Andreea Tamina <taminazamfir@gmail.com>
2026-01-13 00:36:12 +0200

    test cel mai mizer program existent pe lumea asta

--- .local/install/CodedWorkflow.cs
+++ .local/install/CodedWorkflow.cs
@@ -20,7 +20,7 @@ namespace ProiectRPAZamfirTamina
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService)};
+            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
